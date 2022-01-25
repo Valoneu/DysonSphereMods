bc90cb08ec0f31ccfcd510f8ecd4aa680df6c6e5
Valoneu <filip@becicka.cz>
2022-01-25 21:10:42 +0100

    sound

--- CustomWarpSound/Plugin.cs
+++ CustomWarpSound/Plugin.cs
@@ -2,6 +2,8 @@
 using CommonAPI;
 using CommonAPI.Systems;
 using HarmonyLib;
+using System.IO;
+using System.Reflection;
 using UnityEngine;
 
 namespace CustomWarpSound
@@ -15,9 +17,14 @@ namespace CustomWarpSound
         private void Awake()
         {
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-            ProtoRegistry.EditAudio( 112,  "StartUP.mp3", 1, 1, 0, 0 ); // startup
-            ProtoRegistry.EditAudio( 113,  "Working.mp3", 1, 1, 0, 0 ); // running
-            ProtoRegistry.EditAudio( 114,  "SlowDown.wav", 1, 1, 0, 0 ); // slowdown
+            string pluginfolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var key = "customwarpsound";
+            var resources = new ResourceData(PluginInfo.PLUGIN_GUID, key, pluginfolder);
+            resources.LoadAssetBundle(key);
+            ProtoRegistry.AddResource(resources);
+            ProtoRegistry.EditAudio( 112,  "Assets/Audio/StartUP.mp3", 1, 1, 0, 0 ); // startup
+            ProtoRegistry.EditAudio( 113,  "Assets/Audio/Working.wav", 1, 1, 0, 0 ); // running
+            ProtoRegistry.EditAudio( 114,  "Assets/Audio/SlowDown.mp3", 1, 1, 0, 0 ); // slowdown
         }
     }
 }
